1bfbe5cfa49c20169aa5d2db25bdcfa3725dca69
nayankumarmpqaWhistle <nayankumarmpqa@gmail.com>
2023-04-04 22:51:05 +0530

    Serialization practice day 1 in Mypost class

---
+++ TestMavenProject/src/test/java/pojo/Post_Payloads_Pojo.java
@@ -0,0 +1,46 @@
+package pojo;
+
+public class Post_Payloads_Pojo {
+
+    /*
+    "{\n" +
+                "  \"title\": \"foo\",\n" +
+                "  \"body\": \"bar\",\n" +
+                "  \"userId\": \"1\" \n}";
+
+     */
+
+
+    private String title;
+    private String body;
+    private int userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+
+
+
+}

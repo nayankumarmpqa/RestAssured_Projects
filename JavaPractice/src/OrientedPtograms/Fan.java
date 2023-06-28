150d5afa88327ef9f0675fbab06298a8fee18826
nayankumarmpqaWhistle <nayankumarmpqa@gmail.com>
2023-06-28 14:13:34 +0530

    updated the FAN class for more clarity on state and behavior of a class

--- JavaPractice/src/OrientedPtograms/Fan.java
+++ JavaPractice/src/OrientedPtograms/Fan.java
@@ -3,46 +3,41 @@ package OrientedPtograms;
 public class Fan {
     //state or variables
 
-    private String comp;
+    private String brand;
     private double radios;
     private String color;
 
 
-// state that changes too much
+    // state that changes too much
     private boolean isOn;
     private byte speed;  //0 to 5
 
 
     public void switchOn() {
-this.isOn=true;
-
+        this.isOn = true;
     }
 
     public void switchOff() {
-        this.isOn=false;
-        setSpeed((byte)0);
-
+        this.isOn = false;
+        setSpeed((byte) 0);
     }
 
-
-    void setSpeed(byte speed){
-this.speed=speed;
-    }
-    // creation
-   public Fan (String comp, double radios, String color){
-        this.comp= comp;
-                this.radios=radios;
-                        this.color=color;
-
+    void setSpeed(byte speed) {
+        this.speed = speed;
     }
 
+    // object creation using constructor
+    public Fan(String brand, double radios, String color) {
+        this.brand = brand;
+        this.radios = radios;
+        this.color = color;
+    }
 
-    //print a state
-
+    //print a state using toString method
     @Override
     public String toString() {
         return "Fan{" +
-                "comp='" + comp + '\'' +
+                "comp='" + brand + '\'' +
                 ", radios=" + radios +
                 ", color='" + color + '\'' +
                 ", isOn=" + isOn +

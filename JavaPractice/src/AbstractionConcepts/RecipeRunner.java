abaf08974d3096928aa3b66b9757c109f4a3698a
nayankumarmpqaWhistle <nayankumarmpqa@gmail.com>
2023-06-30 13:58:19 +0530

    understanding Abstraction

--- JavaPractice/src/AbstractionConcepts/RecipeRunner.java
+++ JavaPractice/src/AbstractionConcepts/RecipeRunner.java
@@ -10,6 +10,18 @@ public class RecipeRunner {
         abstractRecipe.executeRecipe();
         //abstractRecipe.curryRecipe2(); not allowed due to parent class reference with child class object case
 
+//Another implementation class for abstract class
+        MicrowaveCurryRecipe microwaveRecipe = new MicrowaveCurryRecipe();
+        microwaveRecipe.executeRecipe();
+//MicrowaveCurryRecipe defines what needs to be done in each step.
+// When we invoke the executeRecipe method, the steps are executed in order.
+
+        /*
+        Summary
+            This pattern is called a Template method pattern.
+            You define an abstract class with the order of steps defined.
+            You leave the implementation of each of the steps to the sub classes.
+         */
 
     }
 

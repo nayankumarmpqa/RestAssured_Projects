7c5021e11613f3b39f0b479fea4e0c6999f4307f
nayankumarmpqaWhistle <nayankumarmpqa@gmail.com>
2023-07-22 17:55:50 +0530

    understanding collection framework -List interface

---
+++ JavaPractice/src/Collections/ListInterfaceTest.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInterfaceTest {
+
+    public static void main(String[] args) {
+        List<String> namesList = List.of("Nayan", "Rupali","Myra");
+        List<String> al = new ArrayList<>(namesList);
+        System.out.println(al);
+    }
+
+
+
+
+
+}

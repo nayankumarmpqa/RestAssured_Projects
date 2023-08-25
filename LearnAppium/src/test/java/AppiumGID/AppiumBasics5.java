e7b925828d3947f9bb2659d4c654b2ca21f7ec6b
nayankumarmpqaWhistle <nayankumarmpqa@gmail.com>
2023-08-25 17:11:40 +0530

    Learnign Appium and understanding loactors

--- LearnAppium/src/test/java/AppiumGID/AppiumBasics5.java
+++ LearnAppium/src/test/java/AppiumGID/AppiumBasics5.java
@@ -1,6 +1,8 @@
 package AppiumGID;
 
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -13,7 +15,18 @@ public class AppiumBasics5 extends BeforeEachTest5 {
         // configureAppium(); will be automatically invoked as @Beforeclass method in parent class
 
         // finding and clicking locator
+        // in Appium we have following type of locators
+        // xpath , id,className, accessibilityId, androidUIAutomator
         androidDriver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        androidDriver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+        androidDriver.findElement(By.id("android:id/checkbox")).click();
+        androidDriver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+
+        String alertTitle = androidDriver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitle, "WiFi settings");
+
+        androidDriver.findElement(By.id("android:id/edit")).sendKeys("Nayan wifi");
+        androidDriver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
 
         //tearDown(); will be automatically invoked as @Afterclass method in parent class
     }
